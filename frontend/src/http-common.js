ce4aca6fc0a4b2a4bc7c150f97f7e3394bc96f6f
adityayaduvanshi0001 <adityayaduvanshi0001@gmail.com>
2021-07-12 22:02:05 +0530

    initial commit

---
+++ frontend/src/http-common.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://ap-south-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-reviews-pjloo/service/restaurants/incoming_webhook/",
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
